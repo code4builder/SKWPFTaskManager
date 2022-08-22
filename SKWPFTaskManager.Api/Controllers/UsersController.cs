4c0ba052ba3006dc228af2e6d6d50751186abe5b
Sergey Kuleshov <code4builder@gmail.com>
2022-08-22 18:08:54 +0200

    Added connection from C# Classes to MSSQL via EntityFramework. Created Model Classes

---
+++ SKWPFTaskManager.Api/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SKWPFTaskManager.Api.Models.Data;
+
+namespace SKWPFTaskManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationContext _db;
+        public UsersController(ApplicationContext db)
+        {
+            _db = db;
+        }
+        [HttpPost("create")]
+        public IActionResult CreateUser([FromBody] object userModel)
+        {
+            if (userModel != null)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
