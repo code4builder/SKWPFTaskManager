081bc48778940af754cc24d476a942041211a985
Sergey Kuleshov <code4builder@gmail.com>
2022-08-31 07:17:06 +0200

    Added left control with buttons from Dictionary

--- SKWPFTaskManager.Client/ViewModels/MainWindowViewModel.cs
+++ SKWPFTaskManager.Client/ViewModels/MainWindowViewModel.cs
@@ -1,13 +1,93 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SKWPFTaskManager.Client.ViewModels
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel : BindableBase
     {
+        #region COMMANDS
+        public DelegateCommand OpenMyInfoPageCommand;
+        public DelegateCommand OpenProjectsPageCommand;
+        public DelegateCommand OpenDesksPageCommand;
+        public DelegateCommand OpenTasksPageCommand;
+        public DelegateCommand LogoutCommand;
+        #endregion
 
+        public MainWindowViewModel()
+        {
+            OpenMyInfoPageCommand = new DelegateCommand(OpenMyInfoPage);
+            NavButtons.Add(_userInfoBtnName, OpenMyInfoPageCommand);
+
+            OpenProjectsPageCommand = new DelegateCommand(OpenProjectsPage);
+            NavButtons.Add(_userProjectsBtnName, OpenProjectsPageCommand);
+
+            OpenDesksPageCommand = new DelegateCommand(OpenDesksPage);
+            NavButtons.Add(_userDesksBtnName, OpenDesksPageCommand);
+
+            OpenTasksPageCommand = new DelegateCommand(OpenTasksPage);
+            NavButtons.Add(_userTasksBtnName, OpenTasksPageCommand);
+
+            LogoutCommand = new DelegateCommand(Logout);
+            NavButtons.Add(_logoutBtnName, LogoutCommand);
+        }
+
+        #region PROPERTIES
+
+        private readonly string _userProjectsBtnName = "My ProjectsDesks";
+        private readonly string _userDesksBtnName = "My Desks";
+        private readonly string _userTasksBtnName = "My Tasks";
+        private readonly string _userInfoBtnName = "My Info";
+        private readonly string _logoutBtnName = "Logout";
+
+        private Dictionary<string, DelegateCommand> _navButtons = new Dictionary<string, DelegateCommand>();
+
+        public Dictionary<string, DelegateCommand> NavButtons
+        {
+            get => _navButtons;
+            set 
+            { 
+                _navButtons = value;
+                RaisePropertyChanged(nameof(NavButtons));
+            }
+        }
+        #endregion
+
+        #region METHODS
+        private void OpenMyInfoPage()
+        {
+            ShowMessage(_userInfoBtnName);
+        }
+        private void OpenProjectsPage()
+        {
+            ShowMessage(_userProjectsBtnName);
+        }
+
+        private void OpenDesksPage()
+        {
+            ShowMessage(_userDesksBtnName);
+        }
+
+        private void OpenTasksPage()
+        {
+            ShowMessage(_userTasksBtnName);
+        }
+
+        private void Logout()
+        {
+            ShowMessage(_logoutBtnName);
+        }
+
+        #endregion
+
+        private void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
     }
 }
